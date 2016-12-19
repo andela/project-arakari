9978f98ca2191b6da8a09c2eb90b40ef57a03305
Collins Wekesa <collins@Collinss-MacBook-Pro.local>
2016-12-20 00:34:39 +0300

    [FIXED] style issue.Refactored code.

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,39 @@
 /**
  * This file is dependent on gulp which automate tasks.
- *Author : Collins
+ *Author : Collins 
 
  */
 
+/**
+* Including necesaary libraries for gulp to run.
+* Note: These libraries can be installed via node package manager.
+*/
 var gulp = require('gulp'),
     livereload = require('gulp-livereload'),
     cleanCSS = require('gulp-clean-css'),
     jshint = require('gulp-jshint'),
+    mocha = require('gulp-mocha'),
     nodemon = require('gulp-nodemon'),
     sass = require('gulp-sass'),
     livereload = require('gulp-livereload');
 
-var browserSync1 = require("browser-sync").create();
-var browserSync2 = require("browser-sync").create();
+ // makes browser do a full-page refresh when change is made on static files (.js,.scss,.html) 
 var browserSync = require("browser-sync").create();
 
-gulp.task("default", ["start_server" , 'test_server']);
+/************************************************************************************
+* Initalizes default tasks to be run by gulp                                        *
+* start_server - > this runs to open the application index page on browser          *
+* node_server - > this runs the server.js file.simulates cmd[nodemon server.js]     *
+* or command [node server.js].                                                      *
+* test_server - > this runs the tests in the application.                           *
+* watch - > watches changes in the files listed under it                            *
+* sass - > loads sass files                                                         *
+*css - > loads css files and monitors the files for changes                         *
+* displays output in the terminal.                                                  *
+*************************************************************************************/
+gulp.task("default", ["start_server"  ,'node_server' , 'test_server' , 'watch' ,'sass','css']);
 
-
+// monitors files for changes and reloads web browser
 gulp.task('css', function() {
   gulp.src('./public/css/*.css')
     .pipe(cleanCSS())
@@ -32,6 +47,8 @@ gulp.task('watch', function(){
   gulp.watch('public/views/**', browserSync.reload);
   gulp.watch(['public/js/**', 'app/**/*.js'], browserSync.reload);
   gulp.watch('app/views/**', browserSync.reload);
+  livereload.listen();
+  gulp.watch('./public/css/*.css', ['css']);
 });
 
 gulp.task('sass',function () {
@@ -40,47 +57,30 @@ gulp.task('sass',function () {
   .pipe(gulp.dest('public/css/'))
 });
  
-gulp.task('watch', function() {
-  livereload.listen();
-  gulp.watch('./public/css/*.css', ['css']);
+
+gulp.task('node_server', function(){
+  nodemon({
+    script: 'server.js',
+    ext: 'js',
+    env: { NODE_ENV: 'development' }
+  })
 });
-
-
-gulp.task('js-watch', ['js'], function (done) {
-    browserSync.reload();
-    done();
-});
-
 
 gulp.task('start_server', function () {
     
-    browserSync1.init({
+    browserSync.init({
         server : {
             baseDir: ["./public/views" , "./app/views/includes" , ".public/css"]
         },
-        port : 3002,
-        ui: {
-            port:3040
-        }
+        port : 3009,
     });
 
-    gulp.watch("./public/**//*.{html,js,css}").on("change", browserSync1.reload);
+    gulp.watch("./public/**//*.{html,js,css}").on("change", browserSync.reload);
 });
 
-gulp.task("test_server", function () {
+gulp.task('test_server', function() {
+  gulp.src('test/**/*.js', { read: false })
+  .pipe(mocha({ reporter: 'spec' }))
 
-    browserSync2.init({
-        server: {
-          baseDir: ["./public/src/", "./test"],
-          port: 3070,
-          ui: {
-              port: 3079
-          }
-      }
-    });
-
-    gulp.watch('./test/article/*.js').on("change",browserSync2.reload);
-    gulp.watch('./test/game/*.js').on("change",browserSync2.reload);
-    gulp.watch('./test/user/*.js').on("change",browserSync2.reload);
-    gulp.watch("./public/src/*.js").on("change", browserSync2.reload); 
-}); 
+  gulp.watch('test/**/*.js').on("change" , browserSync.reload);
+});
