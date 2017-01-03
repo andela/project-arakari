bc315e790c18341aad0bab0b4d5494bf23057644
Jackie W. Macharia <jackline.macharia@andela.com>
2017-01-03 14:54:15 +0300

    [Chore #136848659] Move require dotenv to the top

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 /**
  * Module dependencies.
  */
+ require('dotenv').config();
 var express = require('express'),
     fs = require('fs'),
     passport = require('passport'),
     logger = require('mean-logger'),
     io = require('socket.io');
-require('dotenv').config();
 
 /**
  * Main application entry file.
