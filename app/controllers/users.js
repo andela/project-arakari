80b3d48bf6941fbd5ac4ebc4651a5a90d60eb1d2
GathuBoswell <gathudev@gmail.com>
2017-01-06 19:08:19 +0300

    add functionality to add jwt and handle login auth

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,52 +1,61 @@
+var jwt = require('jsonwebtoken'),
+    config = require('../../config/config');
+
+function generateToken(user) {
+    return jwt.sign(user, config.secret, {
+        expiresIn: 10080 // in seconds
+    });
+}
 /**
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-  User = mongoose.model('User');
+    User = mongoose.model('User');
 var avatars = require('./avatars').all();
 
 /**
  * Auth callback
  */
 exports.authCallback = function(req, res, next) {
-  res.redirect('/chooseavatars');
+    res.redirect('/chooseavatars');
 };
 
 /**
  * Show login form
  */
 exports.signin = function(req, res) {
-  if (!req.user) {
-    res.redirect('/#!/signin?error=invalid');
-  } else {
-    res.redirect('/#!/app');
-  }
+    if (!req.user) {
+        res.redirect('/#!/signin?error=invalid');
+    } else {
+        res.redirect('/#!/app');
+    }
 };
 
 /**
  * Show sign up form
  */
 exports.signup = function(req, res) {
-  if (!req.user) {
-    res.redirect('/#!/signup');
-  } else {
-    res.redirect('/#!/app');
-  }
+    if (!req.user) {
+        console.log('Hey');
+        res.redirect('/#!/signup');
+    } else {
+        res.redirect('/#!/app');
+    }
 };
 
 /**
  * Logout
  */
 exports.signout = function(req, res) {
-  req.logout();
-  res.redirect('/');
+    req.logout();
+    res.redirect('/');
 };
 
 /**
  * Session
  */
 exports.session = function(req, res) {
-  res.redirect('/');
+    res.redirect('/');
 };
 
 /** 
@@ -55,134 +64,176 @@ exports.session = function(req, res) {
  * to our Choose an Avatar page.
  */
 exports.checkAvatar = function(req, res) {
-  if (req.user && req.user._id) {
-    User.findOne({
-      _id: req.user._id
-    })
-    .exec(function(err, user) {
-      if (user.avatar !== undefined) {
-        res.redirect('/#!/');
-      } else {
-        res.redirect('/#!/choose-avatar');
-      }
-    });
-  } else {
-    // If user doesn't even exist, redirect to /
-    res.redirect('/');
-  }
+    if (req.user && req.user._id) {
+        User.findOne({
+                _id: req.user._id
+            })
+            .exec(function(err, user) {
+                if (user.avatar !== undefined) {
+                    res.redirect('/#!/');
+                } else {
+                    res.redirect('/#!/choose-avatar');
+                }
+            });
+    } else {
+        // If user doesn't even exist, redirect to /
+        res.redirect('/');
+    }
 
+};
+
+exports.register = function(req, res) {
+    if (req.body.name && req.body.password && req.body.email) {
+        User.findOne({
+            email: req.body.email
+        }).exec(function(err, existingUser) {
+            if (!existingUser) {
+                var user = new User(req.body);
+                // Switch the user's avatar index to an actual avatar url
+                user.avatar = avatars[user.avatar];
+                user.provider = 'local';
+                user.save(function(err) {
+                    if (err) {
+                        return res.render('/#!/signup?error=unknown', {
+                            errors: err.errors,
+                            user: user
+                        });
+                    }
+                    res.status(200).json({
+                        token: 'JWT' + generateToken(user),
+                        user: user
+                    });
+                });
+            } else {
+                return res.send({
+                    message: 'Email exists'
+                });
+            }
+        });
+    } else {
+        return res.send({
+            message: 'You have not filled some fields'
+        });
+    }
 };
 
 /**
  * Create user
  */
-exports.create = function(req, res) {
-  if (req.body.name && req.body.password && req.body.email) {
-    User.findOne({
-      email: req.body.email
-    }).exec(function(err,existingUser) {
-      if (!existingUser) {
-        var user = new User(req.body);
-        // Switch the user's avatar index to an actual avatar url
-        user.avatar = avatars[user.avatar];
-        user.provider = 'local';
-        user.save(function(err) {
-          if (err) {
-            return res.render('/#!/signup?error=unknown', {
-              errors: err.errors,
-              user: user
-            });
-          }
-          req.logIn(user, function(err) {
-            if (err) return next(err);
-            return res.redirect('/#!/');
-          });
+exports.create = function(req, res, next) {
+    if (req.body.name && req.body.password && req.body.email) {
+        User.findOne({
+            email: req.body.email
+        }).exec(function(err, existingUser) {
+            if (!existingUser) {
+                var user = new User(req.body);
+                // Switch the user's avatar index to an actual avatar url
+                user.avatar = avatars[user.avatar];
+                user.provider = 'local';
+                user.save(function(err) {
+                    if (err) {
+                        return res.render('/#!/signup?error=unknown', {
+                            errors: err.errors,
+                            user: user
+                        });
+                    }
+                    console.log('JWT' + generateToken(user));
+                    req.logIn(user, function(err) {
+                        if (err) {
+                            return next(err);
+                        } else {
+                            res.status(201).redirect('/#!/').send({
+                                token: 'JWT' + generateToken(user),
+                                user: user
+                            });
+                        }
+                    });
+                });
+            } else {
+                return res.redirect('/#!/signup?error=existinguser');
+            }
         });
-      } else {
-        return res.redirect('/#!/signup?error=existinguser');
-      }
-    });
-  } else {
-    return res.redirect('/#!/signup?error=incomplete');
-  }
+    } else {
+        return res.redirect('/#!/signup?error=incomplete');
+    }
 };
 
 /**
  * Assign avatar to user
  */
 exports.avatars = function(req, res) {
-  // Update the current user's profile to include the avatar choice they've made
-  if (req.user && req.user._id && req.body.avatar !== undefined &&
-    /\d/.test(req.body.avatar) && avatars[req.body.avatar]) {
-    User.findOne({
-      _id: req.user._id
-    })
-    .exec(function(err, user) {
-      user.avatar = avatars[req.body.avatar];
-      user.save();
-    });
-  }
-  return res.redirect('/#!/app');
+    // Update the current user's profile to include the avatar choice they've made
+    if (req.user && req.user._id && req.body.avatar !== undefined &&
+        /\d/.test(req.body.avatar) && avatars[req.body.avatar]) {
+        User.findOne({
+                _id: req.user._id
+            })
+            .exec(function(err, user) {
+                user.avatar = avatars[req.body.avatar];
+                user.save();
+            });
+    }
+    return res.redirect('/#!/app');
 };
 
 exports.addDonation = function(req, res) {
-  if (req.body && req.user && req.user._id) {
-    // Verify that the object contains crowdrise data
-    if (req.body.amount && req.body.crowdrise_donation_id && req.body.donor_name) {
-      User.findOne({
-        _id: req.user._id
-      })
-      .exec(function(err, user) {
-        // Confirm that this object hasn't already been entered
-        var duplicate = false;
-        for (var i = 0; i < user.donations.length; i++ ) {
-          if (user.donations[i].crowdrise_donation_id === req.body.crowdrise_donation_id) {
-            duplicate = true;
-          }
+    if (req.body && req.user && req.user._id) {
+        // Verify that the object contains crowdrise data
+        if (req.body.amount && req.body.crowdrise_donation_id && req.body.donor_name) {
+            User.findOne({
+                    _id: req.user._id
+                })
+                .exec(function(err, user) {
+                    // Confirm that this object hasn't already been entered
+                    var duplicate = false;
+                    for (var i = 0; i < user.donations.length; i++) {
+                        if (user.donations[i].crowdrise_donation_id === req.body.crowdrise_donation_id) {
+                            duplicate = true;
+                        }
+                    }
+                    if (!duplicate) {
+                        console.log('Validated donation');
+                        user.donations.push(req.body);
+                        user.premium = 1;
+                        user.save();
+                    }
+                });
         }
-        if (!duplicate) {
-          console.log('Validated donation');
-          user.donations.push(req.body);
-          user.premium = 1;
-          user.save();
-        }
-      });
     }
-  }
-  res.send();
+    res.send();
 };
 
 /**
  *  Show profile
  */
 exports.show = function(req, res) {
-  var user = req.profile;
+    var user = req.profile;
 
-  res.render('users/show', {
-    title: user.name,
-    user: user
-  });
+    res.render('users/show', {
+        title: user.name,
+        user: user
+    });
 };
 
 /**
  * Send User
  */
 exports.me = function(req, res) {
-  res.jsonp(req.user || null);
+    res.jsonp(req.user || null);
 };
 
 /**
  * Find user by id
  */
 exports.user = function(req, res, next, id) {
-  User
-    .findOne({
-      _id: id
-    })
-    .exec(function(err, user) {
-      if (err) return next(err);
-      if (!user) return next(new Error('Failed to load User ' + id));
-      req.profile = user;
-      next();
-    });
+    User
+        .findOne({
+            _id: id
+        })
+        .exec(function(err, user) {
+            if (err) return next(err);
+            if (!user) return next(new Error('Failed to load User ' + id));
+            req.profile = user;
+            next();
+        });
 };
