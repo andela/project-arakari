62b622c47478eb55d12f7d591a64a8f8f9fe31ee
andela-pbirir <purity.birir@andela.com>
2017-01-09 23:21:12 +0300

    (feature):Authenticate user
    Add method to authenticate if the user has a token
    
    135842525

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -232,3 +232,33 @@ exports.user = function(req, res, next, id) {
       next();
     });
 };
+
+exports.authToken = function(req, res, next) {
+
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token;
+
+    // decode token
+    if (token) {
+        // verifies secret and checks exp
+        jwt.verify(token, config.secret, function(err, decoded) {
+            if (err) {
+                return res.json({ success: false, message: 'Failed to authenticate token.' });
+            } else {
+                // if everything is good, save to request for use in other routes
+                req.decoded = decoded;
+                next();
+            }
+        });
+
+    } else {
+
+        // if there is no token
+        // return an error
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+
+    }
+};
