197555b371c8c384a8727f2a882bcc5940845377
Collins Wekesa <collins.wekesa@andela.com>
2017-01-17 20:25:31 +0300

    add article model

---
+++ app/models/article.js
@@ -0,0 +1,30 @@
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    config = require('../../config/config'),
+    Schema = mongoose.Schema
+/**
+ * Article Schema
+ */
+var ArticleSchema = new Schema({
+    id: {
+        type: Number
+    },
+    text: {
+        type: String,
+        default: '',
+        trim: true
+    }
+})
+
+ArticleSchema.statics = {
+    load: function(id, cb) {
+        this.findOne({
+            id: id
+        }).select('-_id').exec(cb)
+    }
+}
+
+
+mongoose.model('Article', ArticleSchema)
