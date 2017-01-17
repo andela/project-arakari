9341471861fa6b4aab89190961e016864fee650f
Jimnah Magira <jimnah.magira@andela.com>
2017-01-17 10:22:56 +0300

    Update code styling and fail passing spec

--- test/test.spec.js
+++ test/test.spec.js
@@ -1,6 +1,6 @@
-describe('Users factory', function() {
-  it('has a dummy spec to test 2 + 2', function() {
-    // An intentionally failing test. No code within expect() will never equal 4.
-    expect(8).toEqual(8);
-  });
-});
+describe('Users factory', function () {
+  it('has a dummy spec to test 2 + 2', function () {
+    // An intentionally failing test. No code within expect() will equal 4.
+    expect(8).toEqual(4)
+  })
+})
